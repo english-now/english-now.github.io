18a90f330e79b67fd8e5b80673f4fd1d4c4c1cba
Erik Boesen <erikboesen@erikboesen.com>
2015-09-08 14:32:28 -0400

    Tweaks to styling.

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,39 @@ $(window).scroll(function () {
     });
 });
 
-$(".menuButton").click(function() {
-    $("nav").animate({
-        top: 0
+$(document).ready(function() {
+    var isMenuOpen = false;
+    $(".menuButton").click(function() {
+        if (isMenuOpen === false) {
+            $("nav").animate({
+                top: 0
+            }, 100);
+            isMenuOpen = true;
+        } else {
+            $("nav").animate({
+                top: "100vh"
+            }, 100);
+            isMenuOpen = false;
+        }
     });
+});
+$(window).resize(function() {
+    if (window.innerWidth < 1000) {
+        $("nav").css({
+            top: 0
+        });
+    }
 });
 
 function emailMe(address) {
     console.log("test");
     window.open("mailto:" + address + "@english-now.com");
 }
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-11213935-1', 'auto');
+ga('send', 'pageview');
