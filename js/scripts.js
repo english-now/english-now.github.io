a914a2749c8be67d37f60b7e3ec45b6a8187a4d8
Erik Boesen <erik.boesen@gmail.com>
2015-10-06 10:07:12 -0400

    Add ALL the social buttons!s

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,21 @@ function emailMe(address) {
     window.open("mailto:" + address + "@english-now.com");
 }
 
+$(document).ready(function() {
+    $("#emailShareButton").click(function() {
+        window.open("mailto:?subject=Check out this page!&body=" + window.location.href);
+    });
+    $("#facebookShareButton").click(function() {
+        window.open("https://www.facebook.com/sharer/sharer.php?u=" + window.location.href);
+    });
+    $("#twitterShareButton").click(function() {
+        window.open("https://twitter.com/intent/tweet?text=English+Now!+http%3A%2F%2F" + window.location.href);
+    });
+    $("#googleShareButton").click(function() {
+        window.open("https://plus.google.com/share?url=" + window.location.href);
+    });
+});
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
